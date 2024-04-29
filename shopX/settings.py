432e886f8511ed6f660a21c572996a39d906c978
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2024-04-29 10:37:15 +0100

    feat: deploy 103

--- shopX/settings.py
+++ shopX/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.getenv("DEBUG"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q1igz(9ktxw8-gpqgiq*)!#8mo-31k-enc7he)z^cm@dt$o-fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["django-shopx.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+
+#django-shopx.onrender.com
 
 
 # Application definition
