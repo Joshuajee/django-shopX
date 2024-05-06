65b678581d6531ef05ff17358e79bad29d51e090
Favour Abilogun-Wole Oluwasayo <favyabils125@gmail.com>
2024-05-06 09:08:26 +0100

    changed

--- shops/urls.py
+++ shops/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, product
+from .views import index, product, contact, about, cart
 
 urlpatterns = [ 
     path("", index), 
-    path("product/<int:id>", product)
+    path("product/<int:id>", product),
+    path("about", about),
+    path("contact", contact),
+    path("cart", cart),
 ]
 
 
